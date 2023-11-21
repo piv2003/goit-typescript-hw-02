0dc4682f14bbe319acca9433671b0287bad81638
Ihor Poliakov <piv2003@gmail.com>
2023-11-21 23:31:02 +0200

    Update 1.ts

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -1,8 +1,8 @@
 // Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
 
-let age = 50;
-let name = 'Max';
-let toggle = true;
+let age:number = 50;
+let name:string = 'Max';
+let toggle:boolean = true;
 let empty = null;
 let notInitialize;
 let callback = (a) => { return 100 + a };
