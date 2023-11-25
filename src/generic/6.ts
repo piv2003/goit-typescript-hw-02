254acd6213adb6e25c3f454596fc69c0f8632d06
Ihor Poliakov <piv2003@gmail.com>
2023-11-25 23:01:30 +0200

    Update 6.ts

--- src/generic/6.ts
+++ src/generic/6.ts
@@ -8,14 +8,21 @@
 */
 
 type User = {
-  name: string;
+     name: string;
   surname: string;
-  email: string;
-  password: string;
+    email: string;
+ password: string;
 }
 
-function createOrUpdateUser(initialValues: User) {
+function createOrUpdateUser(initialValues: Partial<User>): User {
   // Оновлення користувача
+  const updatedUser: User = {
+    name: initialValues.name || 'defaultName',
+    surname: initialValues.surname || 'defaultSurname',
+    email: initialValues.email || 'default@mail.com',
+    password: initialValues.password || 'defaultPassword',
+  };
+  return updatedUser;
 }
 
 createOrUpdateUser({ email: 'user@mail.com', password: 'password123' });
