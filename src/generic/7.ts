6aa9c873f45f07485fe2623f6f72eabcd1603362
Ihor Poliakov <piv2003@gmail.com>
2023-11-26 11:45:04 +0200

    Update 7.ts

--- src/generic/7.ts
+++ src/generic/7.ts
@@ -9,8 +9,10 @@ export enum UserRole {
   guest = 'guest',
 }
 
+type UserRoles = Record<UserRole, string>;
+
 // Замініть наступний код на версію за допомогою Record
-const RoleDescription = {
+const RoleDescription: UserRoles = {
   admin: 'Admin User',
   editor: 'Editor User',
   guest: 'Guest User',
